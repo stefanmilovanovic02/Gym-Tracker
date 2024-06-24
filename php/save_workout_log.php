2df4829af2d63e650c9d615cf08898175e8dc820
stefanmilovanovic02 <stefanmilovanovic989@gmail.com>
2024-06-24 15:18:21 +0200

    update

--- php/save_workout_log.php
+++ php/save_workout_log.php
@@ -17,10 +17,16 @@ foreach ($_POST as $key => $value) {
         $exercise_id = str_replace('weight_', '', $key);
         foreach ($value as $index => $weight) {
             $reps = $_POST['reps_' . $exercise_id][$index];
+            $sets = $index + 1; // Set number is the index + 1
+
+            // Convert values to variables
+            $weight_val = $weight;
+            $reps_val = $reps;
+
             // Insert exercise log
             $sql_insert_exercise_log = "INSERT INTO exercise_logs (user_id, workout_id, exercise_id, sets, reps, weight, date) VALUES (?, ?, ?, ?, ?, ?, ?)";
             $stmt_insert_exercise_log = $conn->prepare($sql_insert_exercise_log);
-            $stmt_insert_exercise_log->bind_param('iiiidss', $user_id, $workout_id, $exercise_id, $index + 1, $reps, $weight, $date);
+            $stmt_insert_exercise_log->bind_param('iiiidss', $user_id, $workout_id, $exercise_id, $sets, $reps_val, $weight_val, $date);
             $stmt_insert_exercise_log->execute();
             $stmt_insert_exercise_log->close();
         }
